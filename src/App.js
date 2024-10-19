55ea4dab132ceaae0ec3239b1fbaf981b38c0521
Yassine Ben Zriouil <yassinebenzriouil7@gmail.com>
2024-10-19 15:47:59 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./app.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App({ targetValue }) {
+    const randGenerator = () => Math.floor(Math.random() * 6) + 1;
+
+    const imageConcerned = (number) => {
+        switch (number) {
+            case 1:
+                return "https://www.svgrepo.com/show/334601/dice-1.svg";
+            case 2:
+                return "https://www.svgrepo.com/show/344130/dice-2-fill.svg";
+            case 3:
+                return "https://www.svgrepo.com/show/322175/dice-six-faces-three.svg";
+            case 4:
+                return "https://www.svgrepo.com/show/322172/dice-six-faces-four.svg";
+            case 5:
+                return "https://www.svgrepo.com/show/499117/dice-five.svg";
+            default:
+                return "https://www.svgrepo.com/show/499120/dice-six.svg";
+        }
+    };
+
+    const [randomNumber, setRandomNumber] = useState(randGenerator());
+    const [imageSrc, setImageSrc] = useState(imageConcerned(randomNumber));
+    const [fade, setFade] = useState(false);
+
+    const handleClick = () => {
+        setFade(true);
+        setTimeout(() => {
+            const newNumber = randGenerator();
+            setRandomNumber(newNumber);
+            setImageSrc(imageConcerned(newNumber));
+            setFade(false);
+        }, 300);
+    };
+
+    const initialize = () => {
+        setRandomNumber(0);
+        setImageSrc(
+            "https://www.cranfield-colours.co.uk/wp-content/uploads/2022/01/cranfield-traditional-etching-ink-mid-black.jpg"
+        );
+    };
+
+    return (
+        <>
+            <div className="main_div">
+                <img
+                    className={`img ${fade ? "fade-out" : "fade-in"}`}
+                    src={imageSrc}
+                    alt="Random Number"
+                />
+                <h1 className="number" id="number">
+                    {randomNumber}
+                </h1>
+                <button className="button" onClick={handleClick}>
+                    Roll The Dice
+                </button>
+                <button className="button" onClick={initialize}>
+                    Initialize
+                </button>
+            </div>{" "}
+            {randomNumber === targetValue && (
+                <p className="congratulations">
+                    Congratulations! You Found The Secret Number
+                </p>
+            )}
+        </>
+    );
 }
 
 export default App;
